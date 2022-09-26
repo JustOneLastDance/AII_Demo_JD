e924053d1e138f7c5325f6d5ba71346dc2a9a684
JustinChou <justinchou186@gmail.com>
2022-09-26 22:20:44 +0800

    1. init project

---
+++ AII_Proj_JD/AII_Proj_JD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AII_Proj_JD
+//
+//  Created by JustinChou on 2022/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
